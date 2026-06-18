30902c3c73011b1a379212c2943f0f148af390e7
debojyoti-298 <debojyoti298@gmail.com>
2026-06-18 20:35:02 +0530

    exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -15,12 +16,25 @@ const userSchema = new mongoose.Schema({
         unique:true,
         lowercase:true,
         required: true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value))
+            {
+            throw new Error("Invalid email address" + value);
+
+            }
+        }
     
     },
     password:{
         type:String,
-        required:true,    
+        required:true,   
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password:" + value);
+
+            }
+        }, 
     },
     age:{
         type:Number,
@@ -37,7 +51,13 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl:{
         type:String,
-        default:"https://smsdelhibmw.co.in/wp-content/uploads/2022/02/User-Profile-PNG.png"
+        default:"https://smsdelhibmw.co.in/wp-content/uploads/2022/02/User-Profile-PNG.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL:" + value);
+
+            }
+        },
     },
     about:{
         type:String,
